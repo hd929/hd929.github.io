ee4f0cb58a86135347447855bd66da2cdfc54784
Akatsuki <handuy099@gmail.com>
2024-11-11 15:29:35 +0700

    Đổi mới phong cách

--- dev/script.js
+++ dev/script.js
@@ -2,10 +2,38 @@ let page = document.getElementById('content')
 
 function changePage(dir) {
   page.innerHTML = `
-  <zero-md
-    src="./${dir}.md"
-    css-urls='["./assets/css/style.css", "./assets/css/zero-md.css", "./assets/prism/prism.css"]'
-    no-shadow="true"
-  ></zero-md>
+    <zero-md
+      src="./${dir}.md"
+      no-shadow="true"
+    >
+      <template>
+        <link rel="stylesheet" href="./assets/css/style.css" />
+        <link rel="stylesheet" href="./assets/css/zero-md.css" />
+
+        <style>
+          :host {
+            display: block;
+            position: relative;
+            contain: content;
+          }
+          :host([hidden]) {
+            display: none;
+          }
+        </style>
+
+        <!-- Highlightjs Github theme (prefers dark) -->
+        <link
+          rel="stylesheet"
+          media="(prefers-color-scheme:dark)"
+          href="https://cdn.jsdelivr.net/npm/@highlightjs/cdn-assets@11/styles/github-dark.min.css"
+        />
+
+        <!-- KaTeX styles (needed for math) -->
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0/dist/katex.min.css"
+        />
+      </template>
+    </zero-md>
  `
 }
